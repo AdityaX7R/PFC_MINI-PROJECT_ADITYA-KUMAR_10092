0c1f88d080e49e6302e8836d05bc971c42cf35bb
Aditya Kumar <adikr4477@gmail.com>
2026-01-07 23:54:39 +0530

    Fix formatting and clean up mini_project.c

--- mini_project.c
+++ mini_project.c
@@ -1,35 +1,26 @@
 #include <stdio.h>
-
 int main() {
     // ADITYA KUMAR 10092
     int n, original, digit;
     int sum = 0;
-
     printf("Enter a number: ");
     scanf("%d", &n);
-
     original = n;   
-
     while (n > 0) {
         digit = n % 10;   
-
         int fact = 1;
         for (int i = 1; i <= digit; i++) {
             fact = fact * i;   
         }
-
         sum = sum + fact;  
         n = n / 10;        
     }
-
     if (sum == original)
         printf("Strong Number\n");
     else
         printf("Not Strong\n");
-
     return 0;
 }
-
 
 // output
 
